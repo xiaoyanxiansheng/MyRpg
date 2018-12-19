a07ac85ee54a11a80232469a931647238684a402
xiaoyanxiansheng <360507245@qq.com>
2018-12-20 01:01:12 +0800

    完成UICore部分

--- Assets/WG/Script/UI/UICore.cs
+++ Assets/WG/Script/UI/UICore.cs
@@ -53,6 +53,11 @@ public class UICore : MonoBehaviour {
         public Transform transform;
         public ComponentType componentType = ComponentType.Transform;
         public List<ParamEvent> events = new List<ParamEvent>();
+
+        public Param Clone()
+        {
+            return (Param)MemberwiseClone();
+        }
     }
 
     [System.Serializable]
@@ -87,6 +92,47 @@ public class UICore : MonoBehaviour {
         {
             cacheParam.Add(v);
         }
+
+        foreach(ParamArray v in paramArray)
+        {
+            Param frist = v.first.root;
+            Transform parent = v.parent.transform;
+            if (parent  != null)
+            {
+                cacheParam.Add(v.parent);
+            }
+            int count = parent.childCount;
+            for(int i = 0; i < count; i++)
+            {
+                string rootName = frist.name + (i + 1);
+                string index = "00" + (i + 1);
+                Transform rooTrans = parent.FindChild(index);
+                cacheParam.Add(BindAllWidgetsHelper(frist, rootName, rooTrans));
+                for(int j = 0; j < v.first.childs.Count; j++)
+                {
+                    Param param = v.first.childs[j];
+                    string childName = rootName + "_" + param.name;
+                    Transform childTrans = rooTrans.FindChild(param.transform.name);
+                    cacheParam.Add(BindAllWidgetsHelper(param, childName, childTrans));
+                }
+            }
+        }
+    }
+
+    public Param BindAllWidgetsHelper(Param clone,string name,Transform trans,bool isClone = true)
+    {
+        Param param = null;
+        if (isClone)
+        {
+            param = clone.Clone();
+        }
+        else
+        {
+            param = clone;
+        }
+        param.name = name;
+        param.transform = trans;
+        return param;
     }
     #endregion
 }
